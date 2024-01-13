21ccfc34b7fc306e1996613e34d4a7d860569067
Elfajri Harsuda <elfajri.harsuda@gmail.com>
2024-01-14 06:08:39 +0700

    update template

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
 document.querySelector('#copyright-year').innerText = new Date().getFullYear();
